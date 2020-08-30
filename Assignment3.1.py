e14c57f4e768b30234d9df31e7c2ac42dcd99660
Navyatha <navyatha226@gmail.com>
2020-08-30 16:46:19 +0530

    Assignment 3.1 added

---
+++ Assignment3.1.py
@@ -0,0 +1,9 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate_per_hour = input("Enter rate per hour:")
+r = float(rate_per_hour)
+if h < 40:
+    print(a = 40*r)
+else:
+    b = (h-40)*r*1.5
+    print(40*r+b)
